1a96571c31bc170561c40b7dc04ed26ba73cbe70
jongseong-mac <kjs3870@naver.com>
2021-06-23 15:54:50 +0900

    first commit

---
+++ src/main/java/com/example/travistutorial/TravisTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.travistutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravisTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/travistutorial/TravisTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travistutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
