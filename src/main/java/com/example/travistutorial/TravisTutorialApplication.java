f0f94e550c908efb4357468b84f12052e7ad71af
jongseong-mac <kjs3870@naver.com>
2021-06-25 18:53:51 +0900

    push

--- src/main/java/com/example/travistutorial/TravisTutorialApplication.java
+++ src/main/java/com/example/travistutorial/TravisTutorialApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TravisTutorialApplication {
 
+    public int a = 1;
+
     public static void main(String[] args) {
         SpringApplication.run(TravisTutorialApplication.class, args);
     }
